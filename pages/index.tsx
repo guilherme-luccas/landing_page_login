e6b17983ba188b2db6134aa4bfbb1253e92d664d
Guillherme Costa <guilherme.costa@pipetech.com.br>
2022-03-18 22:23:27 -0300

    image

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ import LockIcon from "@mui/icons-material/Lock";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import GoogleIcon from "@mui/icons-material/Google";
-import logo from "../public/criptoLogo.png";
+import logo from "../public/criptologo.png";
 
 //FORM
 import { useFormik, Form, FormikProvider } from "formik";
